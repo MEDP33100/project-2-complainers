e40ae1bde74f35ef1265dcb8dc24045666bb8bed
Shasuna Lin <162057763+shasunal@users.noreply.github.com>
2025-04-09 18:23:54 -0400

    set up

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/SwTV4Aya)
+
+Guide for group members: https://docs.google.com/document/d/1fHbcXlYq9IDHaUIGLQejdg4r5zwGU_uH0AvyFhqQjok/edit?usp=sharing
+
+
 # MEDP 33100 Project 2: Data Visualization
 ## **Project Members**
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
+// Node fetch
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  
+  try{
+    //makes request to API limit:10
+    const response = await fetch(
+      'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=50&$select=created_date,complaint_type,incident_zip,borough,latitude,longitude'
+    )
+
+    //change response to JSON
+    const data = await response.json();
+
+    //passes the data into template to render
+    res.render('index', {complaints:data});
+    console.log("Data fetched from API:", data);
+
+  }
+
+  catch (err){
+    //if error show message
+    console.error(err);
+    res.render('error', {message: 'API fetch failed', error:err});
+  }
 });
 
 module.exports = router;

98d6f6834792e65a54a70d1982282cb64a07568e
Shasuna Lin <162057763+shasunal@users.noreply.github.com>
2025-04-19 19:30:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
 
 // static files from /public folder files
 app.use(express.static(path.join(__dirname, 'public')));
